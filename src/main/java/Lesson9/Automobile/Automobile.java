0ab5b116bb89c56406e1117f08a7cc31a00d3421
Boris <maestrobo1@mai.ru>
2021-12-07 16:38:00 +0500

    Abstract classes

---
+++ src/main/java/Lesson9/Automobile/Automobile.java
@@ -0,0 +1,13 @@
+package Lesson9.Automobile;
+
+public abstract class Automobile {
+    protected String carName;
+    protected int distance;
+
+    public abstract void move();
+
+    public Automobile(String carName) {
+        this.carName = carName;
+        this.distance = (int) (Math.random() * 1000);
+    }
+}
---
+++ src/main/java/Lesson9/Automobile/Car/Car.java
@@ -0,0 +1,24 @@
+package Lesson9.Automobile.Car;
+
+import Lesson9.Automobile.Automobile;
+
+public class Car extends Automobile {
+
+    private static final String TYPE = "Car";
+    private static final int MAX_DISTANCE = 500;
+
+    public Car(String carName) {
+        super(carName);
+    }
+
+    @Override
+    public void move() {
+        if (distance <= MAX_DISTANCE) {
+            System.out.println(this.carName + ", тип автомобиля: " +
+                    TYPE + ", проехал дистанцию: " + distance + " км.");
+        } else {
+            System.out.println(this.carName + ", тип автомобиля: " +
+                    TYPE + " не сможет проехать дистанцию: " + distance + " км.");
+        }
+    }
+}
