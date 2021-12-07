55457adeaee81e686d4f2f31853fe28913f5ad34
Boris <maestrobo1@mai.ru>
2021-12-07 17:35:38 +0500

    Add new truck

--- src/main/java/Lesson9/Automobile/AutomobileRunner.java
+++ src/main/java/Lesson9/Automobile/AutomobileRunner.java
@@ -6,6 +6,7 @@ import Lesson9.Automobile.Truck.Truck;
 public class AutomobileRunner {
     public static void main(String[] args) {
 
+        Truck ural = new Truck("Ural");
         Truck gaz = new Truck("Gaz");
         Truck uaz = new Truck("UAZ");
         Truck kamaz = new Truck("Kamaz");
@@ -16,7 +17,7 @@ public class AutomobileRunner {
         Car skoda = new Car("Skoda");
         Car lada = new Car("Lada");
 
-        Automobile[] automobiles = new Automobile[]{gaz, uaz, kamaz, belaz, mazda, toyota, opel, skoda, lada};
+        Automobile[] automobiles = new Automobile[]{ural, gaz, uaz, kamaz, belaz, mazda, toyota, opel, skoda, lada};
         for (Automobile automobile : automobiles) {
             automobile.move();
         }
